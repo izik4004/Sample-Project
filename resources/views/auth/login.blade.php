d1c40edb33213f21e9ba5899ac9e928c2c8cb41a
izik4004 <izik4004@gmail.com>
2021-01-19 16:55:53 +0100

    added some pages

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    
+
+            <!-- component -->
+ <div class="flex flex-col justify-center py-24 bg-white sm:px-6 lg:px-8 ">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">
+            Sign into your account
+        </h2>
+        <p class="mt-2 text-sm text-center text-gray-600 max-w">
+            Or
+            <a href="" class="font-medium text-indigo-600 hover:text-indigo-500">
+               Register Here
+            </a>
+        </p>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+        @if (session('status'))
+            
+            {{ session('status')}}
+            </div>
+        @endif
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                    <div>
+                    <label for="email" class="block text-sm font-medium text-gray-700 mt-6">
+                        Email address
+                    </label>
+                    <div class="mt-1">
+                    <input id="email" type="email" name="email" value="" required
+                            class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                   </div>
+                    
+                  
+                    <label for="password" class="block text-sm font-medium text-gray-700 mt-6">
+                        Password
+                    </label>
+                    <div class="mt-1">
+                        <input id="password" type="password" name="password" value="" required
+                            class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                    </div>
+                                 
+                </div>
+               
+                               
+                </div>
+                <div>
+                    <button type="submit"
+                        class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 mt-6 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Login In
+                    </button>
+                </div>
+            </form>
+
+            <div class="mt-6">
+                <div class="relative">
+                    <div class="absolute inset-0 flex items-center">
+                        <div class="w-full border-t border-gray-300"></div>
+                    </div>
+                    <div class="relative flex justify-center text-sm">
+                        <span class="px-2 text-gray-500 bg-white">
+                            Or continue with
+                        </span>
+                    </div>
+                </div>
+
+                <div class="grid  mt-6">
+                    <div>
+                        <a href="#"
+                            class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50">
+                            <span class="sr-only">Sign in with Google</span>
+                            {{-- <svg class="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
+                                    clip-rule="evenodd" />
+                            </svg> --}}
+                            Sign in with Google
+                        </a>
+                    </div>
+
+
+               
+           
+        </div>
+    </div>
+</div>
+    
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Posty</title>
+    </head>
+    <body class="bg-gray-100">
+        <header class="text-gray-600 body-font bg-white">
+            <div class="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+                <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                </svg>
+                <span class="ml-3 text-xl">Tailblocks</span>
+                </a>
+                <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                    <a href="{{ route('home')}}" class="p-3">Home</a>
+                    <a href="{{ route('dashboard')}}" class="p-3">Dashboard</a>
+                    <a href="" class="p-3">Post</a>
+                    </nav>
+                    @auth
+                    <div class="inline-flex items-center">
+                        <a href="" class="p-3">{{auth()->user()->name}}</a>
+                        <form action="{{ route('logout')}}" method="post" class="p-3 inline">
+                            @csrf
+                            <button class="focus:outline-none hover:bg-gray-200 " type="submit">Logout</button>
+                        </form>
+                    </div>
+                    @endauth
+
+                    @guest
+                
+                    <button class="inline-flex items-center bg-gray-100 border-0 focus:outline-none hover:bg-gray-200 rounded text-base mt-2 md:mt-0">
+                        <a href="{{ route('register')}}" class="p-3">Register</a>
+                    </button>
+                    <button class="inline-flex items-center bg-gray-100 border-0 ml-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-2 md:mt-0">
+                        <a href="{{ route('login')}}" class="p-3">Login</a>
+                    </button>
+            
+                @endguest
+            </div>
+        </header>
+
+        @yield('content')
+    </body>
+</html>
